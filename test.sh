d2da031dc256c8a2a125ffbc04084869c2aa61c3
souzamonteiro <roberto@souzamonteiro.com>
2022-08-13 18:16:30 +0000

    Bug fix in reading CSV files

--- test.sh
+++ test.sh
@@ -3,3 +3,6 @@
 #
 # Simple tests.
 #
+
+./bin/maiastatistics.maia --rhodcca test/cryptocoins/BTC-USD-Input.csv "test/cryptocoins/*Close.csv"
+./bin/maiastatistics.maia --plot -o test/cryptocoins.png -i test/cryptocoins.json
