65bb2f4976621ce0126e21208a08f9edf6999a48
souzamonteiro <roberto@souzamonteiro.com>
2022-08-17 20:15:58 +0000

    Bug fix in libraries path

--- build.sh
+++ build.sh
@@ -9,5 +9,7 @@ cp build/maiastatistics.maia bin/
 chmod 755 bin/*
 
 jsdoc -c ./jsdoc.json -d ./docs ./package.json ./src
+jsdoc -c ./jsdoc.json -d ./docs ./maia/dfa/package.json ./maia/dfa
+jsdoc -c ./jsdoc.json -d ./docs ./maia/statistics/package.json ./maia/statistics
 
 #cp manual/* docs/
