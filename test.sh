812422bc50c08ba81b6ddec0edd648408650ebd6
souzamonteiro <roberto@souzamonteiro.com>
2022-08-17 14:51:10 +0000

    Created the statistics library

--- test.sh
+++ test.sh
@@ -4,5 +4,7 @@
 # Simple tests.
 #
 
-./bin/maiastatistics.maia --rhodcca test/cryptocoins/BTC-USD-Input.csv "test/cryptocoins/*Close.csv"
-./bin/maiastatistics.maia --plot -o test/cryptocoins.png -i test/cryptocoins.json
+#./bin/maiastatistics.maia --rhodcca test/cryptocoins/BTC-USD-Input.csv "test/cryptocoins/*Close.csv"
+#./bin/maiastatistics.maia --plot -o test/cryptocoins.png -i test/cryptocoins.json
+./bin/maiastatistics.maia --plot -o test/cryptocoins.pdf -t pdf -i test/cryptocoins.json
+
